cc46658384d12eed9b7be90e8e916d47e757aa5c
Tommy109 <tokutomi@degino.com>
2024-05-25 21:33:38 +0900

    test: add test for blinker

---
+++ src/board.spec.js
@@ -0,0 +1,20 @@
+// ブリンカーパターン1
+const bl1 = [
+    0, 0, 0,
+    1, 1, 1,
+    0, 0, 0
+]
+
+// ブリンカーパターン2
+const bl2 = [
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0
+]
+
+describe('ボードに関するテスト', () => {
+    it('ブリンカーが動作する', () => {
+        const board = new Board()
+        expect(board.nextBoard(bl1)).toEqual(bl2)
+    })
+})
