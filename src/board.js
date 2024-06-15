0be6d154041af943bd81f95b6d7ab1a25498f2ab
Tommy <tokutomi@degino.com>
2024-06-15 21:16:05 +0900

    refactor: modify nextCell

--- src/board.js
+++ src/board.js
@@ -12,6 +12,20 @@ const bl2 = [
     0, 1, 0
 ]
 
+// 事前に数えた活性数1
+const pop1 = [
+    2, 3, 2,
+    1, 2, 1,
+    2, 3, 2
+]
+
+// 事前に数えた活性数2
+const pop2 = [
+    2, 1, 2,
+    3, 2, 3,
+    2, 1, 2
+]
+
 class Board {
     constructor(init) {
         this.state = init
