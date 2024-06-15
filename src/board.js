54f67c83d5f0a1b5d67c0f5f665a60c8362d796d
Tommy <tokutomi@degino.com>
2024-06-15 19:01:45 +0900

    refactor: modify nextCell

--- src/board.js
+++ src/board.js
@@ -27,10 +27,13 @@ class Board {
     // 指定したセルの次世代の状態を取得する
     nextCell(offset) {
         // FIXME: この実装はまだ正しくありません
+        let dest = null
         if (JSON.stringify(this.state) == JSON.stringify(bl1)) {
-            return bl2[offset]
+            dest = bl2
+        } else {
+            dest = bl1
         }
-        return bl1[offset]
+        return dest[offset]
     }
     // 活性数によって次世代を決定する
     nextLife(population, state=null) {
