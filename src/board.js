4827ede1b687238d6c7dd83fb34d67de0855c763
Tommy <tokutomi@degino.com>
2024-06-16 18:14:49 +0900

    feat: modify getCell

--- src/board.js
+++ src/board.js
@@ -65,6 +65,7 @@ class Board {
         if (x < 1) return 0
         if (y < 1) return 0
         if (x > this._size) return 0
+        if (y > this._size) return 0
         return this.state[(y - 1) * this.size + (x - 1)]
     }
 }
